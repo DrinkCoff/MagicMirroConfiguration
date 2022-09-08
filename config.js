6ea95573ac12c33ad25ed1ca08b9eeb68175d7bc
DrinkCoff <37942437+DrinkCoff@users.noreply.github.com>
2022-09-08 17:55:50 -0400

    Update config.js
    With Google Photos

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
  * and https://docs.magicmirror.builders/modules/configuration.html
  */
 let config = {
-	address: "localhost", 	// Address to listen on, can be:
+	address: "0.0.0.0", 	// Address to listen on, can be:
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
@@ -16,7 +16,7 @@ let config = {
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+	ipWhitelist: [], 	// Set [] to allow all IP addresses
 															// or add a specific IPv4 of 192.168.1.5 :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -31,6 +31,8 @@ let config = {
 	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
 	timeFormat: 12,
 	units: "imperial",
+	debug: true,
+	serverOnly:  true,
 	// serverOnly:  true/false/"local" ,
 	// local for armv6l processors, default
 	//   starts serveronly and then starts chrome browser
@@ -74,7 +76,10 @@ let config = {
 		},
 		{
 			module: "compliments",
-			position: "lower_third"
+			position: "bottom_bar",
+			config: {
+				remoteFile: 'https://raw.githubusercontent.com/DrinkCoff/MagicMirroConfiguration/main/compliments.json'
+			}
 		},
 		{
 			module: "weather",
@@ -100,21 +105,45 @@ let config = {
 				apiKey: "d27c116b5ada484fc0a6ea6ec255ec09"
 			}
 		},
+		// {
+			// module: "newsfeed",
+			// position: "bottom_bar",
+			// config: {
+				// feeds: [
+					// {
+						// title: "New York Times",
+						// url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					// }
+				// ],
+				// showSourceTitle: true,
+				// showPublishDate: true,
+				// broadcastNewsFeeds: true,
+				// broadcastNewsUpdates: true
+			// }
+		// },
 		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
-			}
+		  module: "MMM-GooglePhotos",
+		  position: "fullscreen_below",
+		  config: {
+				albums: ["Aahvaan Newborn", "Ganapati"], // Set your album name. like ["My wedding", "family share", "Travle to Paris"]
+				updateInterval: 1000 * 60, // minimum 10 seconds.
+				sort: "new", // "old", "random"
+				uploadAlbum: null, // Only album created by `create_uploadable_album.js`.
+				condition: {
+					fromDate: null, // Or "2018-03", RFC ... format available
+					toDate: null, // Or "2019-12-25",
+					minWidth: null, // Or 400
+					maxWidth: null, // Or 8000
+					minHeight: null, // Or 400
+					maxHeight: null, // Or 8000
+					minWHRatio: null,
+					maxWHRatio: null,
+					// WHRatio = Width/Height ratio ( ==1 : Squared Photo,   < 1 : Portraited Photo, > 1 : Landscaped Photo)
+				},
+				showWidth: 1080, // These values will be used for quality of downloaded photos to show. real size to show in your MagicMirror region is recommended.
+				showHeight: 1920,
+				timeFormat: "YYYY/MM/DD HH:mm", // Or `relative` can be used.
+		  }
 		},
 	]
 };
